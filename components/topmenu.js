86fc527f3b60ec6763136803eb7a51162db2842d
Sergey Tkachuk <sergey.tkachuk@kivano.ru>
2021-05-02 22:45:09 +1000

    Initial props for each page

--- components/topmenu.js
+++ components/topmenu.js
@@ -1,31 +1,44 @@
 import { useState } from "react";
 
-export default function TopMenu() {
+import Link from 'next/link'
+
+export default function TopMenu({ siteInfo }) {
 
     const [shown, setShown] = useState(false);
 
-    const toggleShown = () => {setShown(!shown)};
+    const toggleShown = () => { setShown(!shown) };
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-2">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">Boatplans.cc</a>
+
+                <Link href="/">
+                    <a className="navbar-brand">{siteInfo.siteName}</a>
+                </Link>
+
                 <button className="navbar-toggler" type="button" onClick={toggleShown} aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
+
                 <div className={"collapse navbar-collapse " + (shown ? "show" : "")}>
+
                     <ul className="navbar-nav me-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Row boat plans</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Sail boat plans</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Motor boat plans</a>
-                        </li>
+                        {siteInfo.propulsions.map(propulsion =>
+                            <li className="nav-item" key={propulsion.slug}>
+                                <Link
+                                    href={{
+                                        pathname: '/slug/[slug]/',
+                                        query: { slug: propulsion.slug },
+                                    }}
+                                >
+                                    <a className="nav-link">{propulsion.longName}</a>
+                                </Link>
+                            </li>
+                        )}
                     </ul>
+                    
                 </div>
+
             </div>
         </nav>
     )
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,36 @@
 import '../scss/style.scss'
 
+import App from 'next/app';
+
 import Head from "next/head";
 import TopMenu from "../components/topmenu";
 
-export default function App({ Component, pageProps }) {
+function BoatplansApp({ Component, pageProps }) {
     return <>
         <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#212529" />
         </Head>
-        <TopMenu />
+        <TopMenu siteInfo={pageProps.siteInfo} />
         <Component {...pageProps} />
     </>
-
 }
+
+BoatplansApp.getInitialProps = async (appContext) => {
+    const appProps = await App.getInitialProps(appContext);
+
+    const siteInfo = {
+        siteName: 'Boatplans.cc',
+        propulsions: [
+            {slug: 'row', longName: 'Row boat plans'},
+            {slug: 'sail', longName: 'Sail boat plans'},
+            {slug: 'motor', longName: 'Notor boat plans'},
+        ],
+    };
+
+    appProps.pageProps.siteInfo = siteInfo;
+
+    return {...appProps, siteInfo};
+}
+
+export default BoatplansApp;
