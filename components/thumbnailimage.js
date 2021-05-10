fba07f46883bba0189dcee18eebf1cfed5229b29
Sergey Tkachuk <341734+catavaran@users.noreply.github.com>
2021-05-11 00:55:14 +1000

    Added localization

--- components/thumbnailimage.js
+++ components/thumbnailimage.js
@@ -2,9 +2,9 @@ export default function ThumbnailImage({ image, alt }) {
     return (
         <picture>
             {image.sources.map(source =>
-                <source srcset={source.srcset} type={source.type} key={source.srcset} />
+                <source srcSet={source.srcset} type={source.type} key={source.srcset} />
             )}
-            <img src={image.src} srcset={image.srcset} width={image.width} height={image.height} alt={alt} />
+            <img src={image.src} srcSet={image.srcset} width={image.width} height={image.height} alt={alt} />
         </picture>
     );
 }
--- next.config.js
+++ next.config.js
@@ -3,4 +3,15 @@ module.exports = {
         BACKEND_URL: process.env.BACKEND_URL,
     },
     trailingSlash: true,
+    i18n: {
+        locales: ['en'],
+        defaultLocale: 'en',
+        domains: [
+            {
+                domain: 'boatplans.ru',
+                defaultLocale: 'ru',
+                locales: ['ru'],
+            },
+        ],
+    },
 }
